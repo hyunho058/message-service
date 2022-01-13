facb870cc34f88dbed56c271b521990f692e6d2a
hyunho058 <hyunho058@naver.com>
2022-01-13 23:29:08 +0900

    kafka chatting setting

---
+++ src/main/java/com/example/message/kafkachat/ChattingMessage.java
@@ -0,0 +1,29 @@
+package com.example.message.kafkachat;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+public class ChattingMessage implements Serializable {
+    private String message;
+    private String user;
+    private Long timeStamp;
+
+    private String fileName;
+    private String rawData;
+
+
+    @Builder
+    public ChattingMessage(String fileName, String rawData, String user) {
+
+        this.fileName = fileName;
+        this.rawData = rawData;
+        this.user = user;
+    }
+}
