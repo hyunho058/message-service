54742465a291864e34f3bd1fe5ac35feb4302525
hyunho058 <hyunho058@naver.com>
2022-01-17 21:56:19 +0900

    Change Receiver in ChattingController

--- src/main/java/com/example/message/kafkachat/ChattingController.java
+++ src/main/java/com/example/message/kafkachat/ChattingController.java
@@ -6,7 +6,6 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sound.midi.Receiver;
 import java.util.List;
 
 @RestController
